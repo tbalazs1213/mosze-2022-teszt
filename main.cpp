38fda20f82d5499f35b312eb418a445736a63c46
tbalazs1213 <83310872+tbalazs1213@users.noreply.github.com>
2022-09-09 14:33:10 +0200

    Changes.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];    //NELEMENTS -> N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'  //'' -> "" + ;
+    for (int i = 0;)        //(int i = 0;) -> (int i = 1; i <= N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)     //(int i = 0; i; i++) -> (int i = 0; i < N_ELEMENTS ; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"   //<< b[i] << std::cout;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;      //int atlag; -> double atlag = 0;
+    for (int i = 0; i < N_ELEMENTS, i++)    //(int i = 0; i < N_ELEMENTS, i++) -> (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i]   //;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
